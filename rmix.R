cd23e3e7bd92672cc961980cfc173f59f630e676
kSzpyt <karolo.szpyt@gmail.com>
2018-04-05 00:01:48 +0200

    simplification

--- rmix.R
+++ rmix.R
@@ -4,17 +4,17 @@ rmix <- function(n, family_1, par_1, family_2, par_2, p)
   pp <- sample(c(1,2), n, replace = TRUE, prob = c(p, 1-p))
   # set.seed(10)
   if(family_1 == "norm"){
-    r1 <- sapply(rep(1, length(pp[pp == 1])), paste0("r", family_1), mean = par_1[1], sd = par_1[2])
+    r1 <- rnorm(length(pp[pp == 1]), mean = par_1[1], sd = par_1[2])
   } 
   else if(family_1 == "pois"){
-    r1 <- sapply(rep(1, length(pp[pp == 1])), paste0("r", family_1), par_1)
+    r1 <- rpois(length(pp[pp == 1]), par_1)
   }
   #set.seed(10)
   if(family_2 == "norm"){
-    r2 <- sapply(rep(1, length(pp[pp == 2])), paste0("r", family_2), mean = par_2[1], sd = par_2[2])
+    r2 <-  rnorm(length(pp[pp == 2]), mean = par_1[1], sd = par_1[2])
   } 
   else if(family_2== "pois"){
-    r2 <- sapply(rep(1, length(pp[pp == 2])), paste0("r", family_2), par_2)
+    r2 <- rpois(length(pp[pp == 2]), par_1)
   }
   
   r <- c(r1, r2)
@@ -22,3 +22,4 @@ rmix <- function(n, family_1, par_1, family_2, par_2, p)
   
 }
 
+rmix(10, "norm", c(1, 5), "norm", c(100, 50), 0.2)
