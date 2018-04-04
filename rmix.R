cb01c64f95a7115a2bb38de466a8139ed8e1868c
kSzpyt <karolo.szpyt@gmail.com>
2018-04-05 00:55:57 +0200

    correction

--- rmix.R
+++ rmix.R
@@ -11,15 +11,13 @@ rmix <- function(n, family_1, par_1, family_2, par_2, p)
   }
   #set.seed(10)
   if(family_2 == "norm"){
-    r2 <-  rnorm(length(pp[pp == 2]), mean = par_1[1], sd = par_1[2])
+    r2 <-  rnorm(length(pp[pp == 2]), mean = par_2[1], sd = par_2[2])
   } 
   else if(family_2== "pois"){
-    r2 <- rpois(length(pp[pp == 2]), par_1)
+    r2 <- rpois(length(pp[pp == 2]), par_2)
   }
   
   r <- c(r1, r2)
   return(r)
   
 }
-
-rmix(10, "norm", c(1, 5), "norm", c(100, 50), 0.2)
