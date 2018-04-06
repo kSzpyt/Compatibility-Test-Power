0e5297323a76146963d8b080ae6cc2f2184ae266
kSzpyt <karolo.szpyt@gmail.com>
2018-04-06 22:43:33 +0200

    Upgrade

--- rmix.R
+++ rmix.R
@@ -1,23 +1,31 @@
-
 rmix <- function(n, family_1, par_1, family_2, par_2, p)
 {
+  set.seed(10)
   pp <- sample(c(1,2), n, replace = TRUE, prob = c(p, 1-p))
-  # set.seed(10)
-  if(family_1 == "norm"){
-    r1 <- rnorm(length(pp[pp == 1]), mean = par_1[1], sd = par_1[2])
-  } 
-  else if(family_1 == "pois"){
-    r1 <- rpois(length(pp[pp == 1]), par_1)
-  }
-  #set.seed(10)
-  if(family_2 == "norm"){
-    r2 <-  rnorm(length(pp[pp == 2]), mean = par_2[1], sd = par_2[2])
-  } 
-  else if(family_2== "pois"){
-    r2 <- rpois(length(pp[pp == 2]), par_2)
-  }
+  n1 <- length(pp[pp==1])
+  n2 <- length(pp[pp==2])
   
-  r <- c(r1, r2)
-  return(r)
+  set.seed(10)
+  eval(parse(text = paste0("fun1 <- r", family_1)))
+  r1 <- do.call(fun1, as.list(c(n1, par_1)))
+  
+  set.seed(10)
+  eval(parse(text = paste0("fun2 <- r", family_2)))
+  r2 <- do.call(fun2, as.list(c(n2, par_2)))
+  
+  return(c(r1,r2))
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
